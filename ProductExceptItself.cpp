61ee4448e1b7f483fca40c46ddcd52c38c1aedae
Saiyam Jain <jimisierra2@gmail.com>
2022-04-18 17:48:28 +0530

    Product of Array Except Itself

---
+++ ProductExceptItself.cpp
@@ -0,0 +1,26 @@
+// Given an integer array nums, return an array answer such that answer[i] is equal
+//  to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the
+// division operation.
+
+// Approach: Take product of left side of array then right side
+vector<int> productExceptSelf(vector<int> &nums)
+{
+    vector<int> v(nums.size(), 1);
+    int p = 1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        v[i] = p;
+        p = p * nums[i];
+    }
+    p = 1;
+    for (int i = nums.size() - 1; i < count; i++)
+    {
+        v[i] *= p;
+        p = p * nums[i];
+    }
+    return v;
+}
